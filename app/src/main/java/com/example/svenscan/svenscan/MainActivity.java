b3b347523b560ded0e245f76c4646f575d2bb2ba
Felix Jansson <felixja@student.chalmers.se>
2016-09-20 17:10:10 +0200

    Sound plays when button pressed, need refactor to own file

--- app/src/main/java/com/example/svenscan/svenscan/MainActivity.java
+++ app/src/main/java/com/example/svenscan/svenscan/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.svenscan.svenscan;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button one = (Button)this.findViewById(R.id.audio);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.swosh1);
+        one.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+
     }
 }
