d5fb3168993d838ab686926786fe62f5996abd80
Felix Jansson <felixja@student.chalmers.se>
2016-09-22 16:46:54 +0200

    2.4 Play fixed sound from fixed sources

--- app/src/main/java/com/example/svenscan/svenscan/MainActivity.java
+++ app/src/main/java/com/example/svenscan/svenscan/MainActivity.java
@@ -1,26 +1,23 @@
 package com.example.svenscan.svenscan;
 
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button one = (Button)this.findViewById(R.id.audio);
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.swosh1);
-        one.setOnClickListener(new View.OnClickListener(){
-
-            public void onClick(View v) {
-                mp.start();
-            }
-        });
-
     }
+
+
+    public void playSound(View view){
+        Intent tmp = new Intent(this, PlayPronounciation.class);
+        startActivity(tmp);
+    }
+
 }
---
+++ app/src/main/java/com/example/svenscan/svenscan/PlayPronounciation.java
@@ -0,0 +1,41 @@
+package com.example.svenscan.svenscan;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class PlayPronounciation extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    private MediaPlayer swoshPlayer;
+    private MediaPlayer giggityPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_pronunciation);
+        final Button swosh = (Button)findViewById(R.id.swosh);
+        mediaPlayer = new MediaPlayer();
+        swoshPlayer = MediaPlayer.create(this ,R.raw.swosh1);
+        giggityPlayer = MediaPlayer.create(this, R.raw.giggity);
+        Button giggity = (Button)findViewById(R.id.giggity);
+        swosh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                swoshPlayer.start();
+            }
+        });
+
+        giggity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                giggityPlayer.start();
+            }
+        });
+    }
+
+}
