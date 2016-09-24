f14cc90b631b636e43b475c2c118e3eee67d958c
SamuelHakansson <samuel.hakansson@live.com>
2016-09-25 01:16:35 +0200

    if granted access, recordPronunciation activity is now available but not functional

--- app/src/main/java/com/example/svenscan/svenscan/MainActivity.java
+++ app/src/main/java/com/example/svenscan/svenscan/MainActivity.java
@@ -7,16 +7,18 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-
     public void playSound(View view){
-        Intent tmp = new Intent(this, PlayPronounciation.class);
+        Intent tmp = new Intent(this, PlayPronunciation.class);
+        startActivity(tmp);
+    }
+    public void recordPronunciation(View view){
+        Intent tmp = new Intent(this, RecordPronunciation.class);
         startActivity(tmp);
     }
 
--- app/src/main/java/com/example/svenscan/svenscan/RecordPronunciation.java
+++ app/src/main/java/com/example/svenscan/svenscan/RecordPronunciation.java
@@ -1,26 +1,46 @@
 package com.example.svenscan.svenscan;
 
 import android.media.MediaRecorder;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecordPronunciation extends AppCompatActivity {
+    final String filePath = MediaStore.Audio.Media.DATA;
+    private boolean isRecording = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_pronunciation);
-        MediaRecorder mediaRecorder = new MediaRecorder();
+        final MediaRecorder mediaRecorder = new MediaRecorder();
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mediaRecorder.setMaxDuration(5000);
         mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        mediaRecorder.setOutputFile("/sdcard/sound/" + "getInput");
+        mediaRecorder.setOutputFile(filePath);
         try {
             mediaRecorder.prepare();
         } catch (Exception e){
             e.printStackTrace();
         }
+        final Button recordButton = (Button)findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(!isRecording) {
+                    mediaRecorder.start();
+                    isRecording = true;
+                } else {
+                    mediaRecorder.stop();
+                    isRecording = false;
+                }
+
+            }
+        });
     }
+
 
 }
