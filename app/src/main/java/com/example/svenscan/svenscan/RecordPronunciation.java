d1efa3ab8e127ba22c4b9fbe6426c0c08a847a9a
SamuelHakansson <samuel.hakansson@live.com>
2016-09-25 03:20:21 +0200

    name and record file

--- app/src/main/java/com/example/svenscan/svenscan/RecordPronunciation.java
+++ app/src/main/java/com/example/svenscan/svenscan/RecordPronunciation.java
@@ -2,15 +2,18 @@ package com.example.svenscan.svenscan;
 
 import android.media.MediaRecorder;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class RecordPronunciation extends AppCompatActivity {
-    final String filePath = MediaStore.Audio.Media.DATA;
-    final String PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";;
+    final String PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
     private boolean isRecording = false;
 
     @Override
@@ -21,29 +24,39 @@ public class RecordPronunciation extends AppCompatActivity {
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mediaRecorder.setMaxDuration(5000);
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-        mediaRecorder.setOutputFile(PATH); // + "/" + "getInput" + ".3gp"
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
-        try {
-            mediaRecorder.prepare();
-        } catch (Exception e){
-            e.printStackTrace();
+        File folder = new File(Environment.getExternalStorageDirectory() +
+                File.separator + "SvenScan");
+        if (!folder.exists()) {
+            folder.mkdir();
         }
-        final Button recordButton = (Button)findViewById(R.id.recordButton);
-        recordButton.setOnClickListener(new View.OnClickListener(){
+
+
+        final Button recordButton = (Button) findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
-                if(!isRecording) {
+                EditText swedishWord = (EditText)findViewById(R.id.swedishWord);
+                String filename = swedishWord.getText().toString();
+                System.out.println("Filename = " + filename);
+                mediaRecorder.setOutputFile(PATH + "/SvenScan/" + filename + ".3gp");
+
+                try {
+                    mediaRecorder.prepare();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (!isRecording) {
                     mediaRecorder.start();
                     isRecording = true;
                 } else {
                     mediaRecorder.stop();
                     isRecording = false;
                 }
-
             }
         });
     }
-
-
 }
