8d63d1fa97ff074a35a88e8145f2c3faaa4621c4
Jesper Lindström <raxezdev@gmail.com>
2016-09-25 19:05:34 +0200

    Use Lambas, Java 1.8

--- app/src/main/java/com/example/svenscan/svenscan/Camera.java
+++ app/src/main/java/com/example/svenscan/svenscan/Camera.java
@@ -20,27 +20,21 @@ public class Camera {
     }
 
     public void start() {
-        permission.require(Manifest.permission.CAMERA, new PermissionManager.ISuccessHandler() {
-            @Override
-            public void call() {
-                permission.require(Manifest.permission.WRITE_EXTERNAL_STORAGE, new PermissionManager.ISuccessHandler() {
-                    @Override
-                    public void call() {
-                        File saveFolder = new File(activity.getApplicationContext().getFilesDir().getAbsolutePath(), "MaterialCamera Sample");
+        permission.require(Manifest.permission.CAMERA, () -> {
+            permission.require(Manifest.permission.WRITE_EXTERNAL_STORAGE, () -> {
+                File saveFolder = new File(activity.getApplicationContext().getFilesDir().getAbsolutePath(), "MaterialCamera Sample");
 
-                        if (!saveFolder.exists() && !saveFolder.mkdirs())
-                            throw new RuntimeException("Unable to create save directory, make sure WRITE_EXTERNAL_STORAGE permission is granted.");
+                if (!saveFolder.exists() && !saveFolder.mkdirs())
+                    throw new RuntimeException("Unable to create save directory, make sure WRITE_EXTERNAL_STORAGE permission is granted.");
 
-                        new MaterialCamera(activity)
-                                .stillShot()
-                                .saveDir(saveFolder)
-                                .start(CAMERA_RQ);
-                    }
-                });
-            }
+                new MaterialCamera(activity)
+                        .stillShot()
+                        .saveDir(saveFolder)
+                        .start(CAMERA_RQ);
+            });
         });
     }
-    
+
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Received recording or error from MaterialCamera
         if (requestCode == CAMERA_RQ) {
