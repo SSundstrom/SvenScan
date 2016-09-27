9cd7ca84fcea4a12729bf404b45e20be124c38ea
Jesper Lindström <raxezdev@gmail.com>
2016-09-27 12:46:06 +0200

    Progress on implementing Firebase

--- app/src/main/java/com/example/svenscan/svenscan/repositories/WordRepository.java
+++ app/src/main/java/com/example/svenscan/svenscan/repositories/WordRepository.java
@@ -1,19 +1,51 @@
 package com.example.svenscan.svenscan.repositories;
 
+import android.util.Log;
+
 import com.example.svenscan.svenscan.models.Word;
 
 import com.example.svenscan.svenscan.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class WordRepository {
-
+    private DatabaseReference database;
     private HashMap<String, Word> wordMap;
     private static int index;
 
     public WordRepository() {
         wordMap = new HashMap<>();
-        createWords();
+
+        FirebaseDatabase firebase = FirebaseDatabase.getInstance();
+        database = firebase.getReference("words");
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot listSnapshot) {
+                wordMap = (HashMap<String, Word>) listSnapshot.getValue();
+                Log.d("WordRepository", "size: " + wordMap.size());
+
+                Iterator<Map.Entry<String, Word>> mapIterator = wordMap.entrySet().iterator();
+                while (mapIterator.hasNext()) {
+                    String word = mapIterator.next().getKey();
+                    Log.d("WordRepository", "Added " + word);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("WordRepository", "Failed to read value.", error.toException());
+            }
+        });
     }
 
     public void addWord(String id, Word word) {
@@ -39,14 +71,5 @@ public class WordRepository {
 
     public Word getWordFromID(String id) {
         return wordMap.get(id.toUpperCase());
-    }
-
-    private void createWords() {
-        wordMap.put("BORD", new Word("BORD", "", R.raw.giggity));
-        wordMap.put("HEJ", new Word("HEJ", "", R.raw.giggity));
-        wordMap.put("STOL", new Word("STOL", "", R.raw.giggity));
-        wordMap.put("NAKEN", new Word("NAKEN", "", R.raw.giggity));
-        wordMap.put("JA", new Word("JA", "", R.raw.giggity));
-        wordMap.put("OCKSÅ", new Word("OCKSÅ", "", R.raw.giggity));
     }
 }
