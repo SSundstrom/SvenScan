d7e6312cacd97f66926023ad55d58bbbb6f6c657
Jesper Lindström <raxezdev@gmail.com>
2016-09-27 10:11:35 +0200

    Added wordmanager again

---
+++ app/src/main/java/com/example/svenscan/svenscan/WordManager.java
@@ -0,0 +1,34 @@
+package com.example.svenscan.svenscan;
+
+import java.util.HashMap;
+
+public class WordManager {
+
+    private HashMap<String, Word> wordMap;
+    private static int index;
+
+    public WordManager() {
+        wordMap = new HashMap<>();
+    }
+
+    public void addWord(String id, Word word) {
+        if (wordMap.containsKey(id)) return;
+        wordMap.put(id.toUpperCase(), word);
+        index++;
+    }
+
+    public boolean toggleFavorite(String word) {
+
+        word = word.toUpperCase();
+        if (!wordMap.containsKey(word)) {
+            addWord(word, new Word(word, "", this.hashCode()+index));
+        }
+        wordMap.get(word).setFavorite(!wordMap.get(word).isFavorite());
+
+        return wordMap.get(word).isFavorite();
+    }
+
+    public Word getWordFromID(String id) {
+        return wordMap.get(id.toUpperCase());
+    }
+}
