c9670d77ca2440dd9c5b709b935b0dc5b9adbe02
SamuelHakansson <samuel.hakansson@live.com>
2016-09-27 10:40:47 +0200

    fixed mic permission

--- app/src/main/java/com/example/svenscan/svenscan/RecordPronunciation.java
+++ app/src/main/java/com/example/svenscan/svenscan/RecordPronunciation.java
@@ -1,5 +1,6 @@
 package com.example.svenscan.svenscan;
 
+import android.Manifest;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -8,22 +9,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.svenscan.svenscan.services.permission.PermissionManager;
+
 import java.io.File;
 
 public class RecordPronunciation extends AppCompatActivity {
     final String PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
     private boolean isRecording = false;
+    private MediaRecorder mediaRecorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_pronunciation);
 
-        final MediaRecorder mediaRecorder = new MediaRecorder();
+        PermissionManager permissionManager = new PermissionManager(this);
+        permissionManager.require(Manifest.permission.RECORD_AUDIO, ()->{
+
+            mediaRecorder = new MediaRecorder();
+
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mediaRecorder.setMaxDuration(5000);
         mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        });
 
         File folder = new File(Environment.getExternalStorageDirectory() +
                 File.separator + "SvenScan");
