469b1f91dbf5adb9f9f2d8685337a373bc2f26d5
Lisa Larsson <lislar@student.chalmers.se>
2016-10-02 17:49:38 +0200

    Saves favorites permanentlyish

--- app/src/main/java/com/example/svenscan/svenscan/repositories/FavoriteWordRepository.java
+++ app/src/main/java/com/example/svenscan/svenscan/repositories/FavoriteWordRepository.java
@@ -3,6 +3,7 @@ package com.example.svenscan.svenscan.repositories;
 import com.example.svenscan.svenscan.models.Word;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class FavoriteWordRepository {
@@ -10,9 +11,13 @@ public class FavoriteWordRepository {
     private List<String> favorites = new ArrayList<String>();
 
     public void addFavorite(String word){
-        if(favorites.contains(word.toUpperCase())){
+        if(!favorites.contains(word.toUpperCase())){
             favorites.add(word.toUpperCase());
         }
+    }
+
+    public void addSetToFavorites(HashSet<String> set){
+        favorites.addAll(set);
     }
     
     public void removeFavorite(String word){
@@ -38,10 +43,5 @@ public class FavoriteWordRepository {
 
     public List<String> getFavorites(){
         return this.favorites;
-    }
-
-    //TODO not done!
-    public void setFavorites(){
-
     }
 }
