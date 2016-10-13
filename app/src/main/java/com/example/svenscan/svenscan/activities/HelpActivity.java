e1c6ef10614996223668d96010d8aa41da709e41
Lisa Larsson <lislar@student.chalmers.se>
2016-10-13 12:56:08 +0200

    text removed from help viewom help view

--- app/src/main/java/com/example/svenscan/svenscan/activities/HelpActivity.java
+++ app/src/main/java/com/example/svenscan/svenscan/activities/HelpActivity.java
@@ -25,11 +25,10 @@ public class HelpActivity extends AppIntro2 {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //TODO values
-        addSlide(AppIntroFragment.newInstance(getString(R.string.help1), "", R.drawable.help_1 , Color.parseColor("#99ccff")));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.help2), "", R.drawable.help_2 , Color.parseColor("#77aaff")));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.help3), "", R.drawable.help_3 , Color.parseColor("#5588ff")));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.help4), "", R.drawable.help_4 , Color.parseColor("#3366ff")));
+        addSlide(AppIntroFragment.newInstance("", "", R.drawable.help_1 , getResources().getColor(R.color.help1)));
+        addSlide(AppIntroFragment.newInstance("", "", R.drawable.help_2 , getResources().getColor(R.color.help2)));
+        addSlide(AppIntroFragment.newInstance("", "", R.drawable.help_3 , getResources().getColor(R.color.help3)));
+        addSlide(AppIntroFragment.newInstance("", "", R.drawable.help_4 , getResources().getColor(R.color.help4)));
 
         showSkipButton(true);
         setProgressButtonEnabled(true);
