fcd2649a79d06c4f8efe623d653111f1096a83a4
SamuelHakansson <samuel.hakansson@live.com>
2016-10-19 10:40:02 +0200

    changed processing ocr to laddar

--- app/src/test/java/com/example/svenscan/svenscan/us8PointsTest.java
+++ app/src/test/java/com/example/svenscan/svenscan/us8PointsTest.java
@@ -1,14 +1,7 @@
 package com.example.svenscan.svenscan;
 
-import android.app.Activity;
-
 import com.example.svenscan.svenscan.models.Points;
-import com.example.svenscan.svenscan.utils.IProgressManager;
-import com.example.svenscan.svenscan.utils.ProgressManager;
-
 import org.junit.Test;
-
-import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
 public class us8PointsTest {
